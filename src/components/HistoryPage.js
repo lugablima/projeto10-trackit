34a054bf3dc9d2b94793ff96b069c320bbe58366
LucasLima <lugablima@gmail.com>
2022-05-28 10:36:14 -0300

    Layout da tela Histórico e ajustes no código implementados

--- src/components/HistoryPage.js
+++ src/components/HistoryPage.js
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+
 export default function HistoryPage() {
-    return (
-        <h1>Eu sou o HistoryPage!</h1>
-    );
+  return (
+    <Container>
+      <h6>Histórico</h6>
+      <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+  margin: 70px 0;
+  padding: 28px 21px 57px 19px;
+  font-family: "Lexend Deca", sans-serif;
+  font-weight: 400;
+
+  & > h6 {
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126ba5;
+    margin-bottom: 11px;
+  }
+
+  & > p {
+    font-size: 17.976px;
+    line-height: 22px;
+    color: #666666;
+    margin-top: 6px;
+  }
+`;
--- src/functions/validatePhoto.js
+++ src/functions/validatePhoto.js
@@ -1,5 +1,6 @@
 function validateURL(strURL) {
   let res = strURL?.match(
+    // eslint-disable-next-line
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
   );
   return res !== null;
